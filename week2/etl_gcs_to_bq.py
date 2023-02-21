ccf63b340fcd12308034be0d2d962b64ec8c7d6c
Shahmir <sham9871@gmail.com>
2023-02-21 21:23:49 +0800

    modified ingesting data to bq code

--- week2/etl_gcs_to_bq.py
+++ week2/etl_gcs_to_bq.py
@@ -17,29 +17,43 @@ def etl_parent_flow(months: list[int] = [6,7], colour: str = "yellow", year: int
 def etl_gcs_to_bq(month: int, colour: str, year: int):
   """Main flow to upload data from GCS to BigQuery"""
   path = extract_from_gcs(colour, year, month)
-  df = transform_from_gcs(path)
-  load_to_bq(df, f"taxi_data.{colour}")
+  upload_direct(path, colour)
+  # df = transform_from_gcs(path)
+  # load_to_bq(df, f"taxi_data_sg.{colour}")
   
 @task(name="extract_from_gcs", retries=3, log_prints=True, tags="extract_gcs")
 def extract_from_gcs(colour: str, year: int, month: int) -> str:
   """Download trip data from GCS bucket to local machine, returns path where file is saved"""
-  gcs_path = f"{colour}_tripdata_{year}-{month:02d}.parquet"
+  filename = f"{colour}_tripdata_{year}-{month:02d}.parquet"
+  gcs_path = f"{colour}/{filename}"
   gcs_block: GcsBucket = GcsBucket.load("taxi-gcp")
     
-  save_path = f"./week2/data/{gcs_path}"
-  gcs_block.get_directory(from_path=gcs_path, local_path=save_path)
-  print(f"saved file in {save_path}")
+  save_path = f"./week2/files/{filename}"
+  gcs_block.download_object_to_path(from_path=gcs_path, to_path=save_path)
+  print(f"downloading {gcs_path} from GCS to {save_path}")
   return save_path
+
+@flow(name="upload_direct", log_prints=True)
+def upload_direct(path: str, colour: str):
+  """Directly uploads parquet file to BigQuery by reading parquet file in batches"""
+  parquet_file = pq.ParquetFile(path)
+  for batch in parquet_file.iter_batches(256000):
+    df = batch.to_pandas()
+    load_to_bq(df, f"taxi_data_sg.{colour}")
 
 @task(name="transform data", log_prints=True, tags="transform_bq")
 def transform_from_gcs(path: str) -> pd.DataFrame:
   """Data cleaning, returns dataframe"""
-  df = pd.read_parquet(path)
-  print("Dataset rows:", len(df))
-  print(f"Pre-transform missing passenger count: {df['passenger_count'].isna().sum()}")
-  df.dropna(subset=["passenger_count"], inplace=True) # or fillna(0)
-  print(f"Post-transform missing passenger count: {df['passenger_count'].isna().sum()}")
+  parquet_file = pq.ParquetFile(path)
+  batches = [batch.to_pandas() for batch in parquet_file.iter_batches()]
+  df = pd.concat(batches)
   return df
+  # df = pd.read_parquet(path)
+  # print("Dataset rows:", len(df))
+  # print(f"Pre-transform missing passenger count: {df['passenger_count'].isna().sum()}")
+  # df.dropna(subset=["passenger_count"], inplace=True) # or fillna(0)
+  # print(f"Post-transform missing passenger count: {df['passenger_count'].isna().sum()}")
+  # return df
 
 @task(name="load_to_bq", log_prints=True, tags="load_bq")
 def load_to_bq(df: pd.DataFrame, to_path_upload: str):
@@ -61,5 +75,40 @@ def etl_gcs_to_bq_large():
     print("Dataset rows:", parquet_file.metadata.num_rows)
 
 if __name__ == "__main__":
-  etl_parent_flow(months=[2,3], colour="yellow", year=2019)
+  print("hii")
+  # etl_parent_flow(months=[2], colour="yellow", year=2019)
+  # etl_parent_flow(months=list(range(1,13)), colour="yellow", year=2019)
+  # etl_parent_flow(months=list(range(1,13)), colour="yellow", year=2020)
+  
+  
+  # etl_parent_flow(months=list(range(12,13)), colour="green", year=2019)
+  # etl_parent_flow(months=list(range(7,13)), colour="green", year=2020)
   # etl_gcs_to_bq_large()
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+def fix():
+  # month 4,9,11,12 2019 ... 6 for 2020 ONLY GREEN
+  months = [6]
+  for month in months:
+    savepath = f"./week2/files/green_tripdata_2020-{month:02d}.parquet"
+    df = pd.read_parquet(savepath)
+    df["congestion_surcharge"].fillna(0, inplace=True)
+    df['tolls_amount'] = df['tolls_amount'].astype('int64')
+    df['congestion_surcharge'] = df['congestion_surcharge'].astype('int64')
+    print("month", month)
+    gcp_credentials_block: GcpCredentials = GcpCredentials.load("gcp-credentials")
+    df.to_gbq(
+      destination_table="taxi_data_sg.green",
+      project_id="data-eng-first",
+      credentials=gcp_credentials_block.get_credentials_from_service_account(),
+      chunksize=100_000,
+      if_exists="append"
+    )
--- week2/etl_web_to_gcs.py
+++ week2/etl_web_to_gcs.py
@@ -54,14 +54,25 @@ def write_local(df: pd.DataFrame, filepath: str):
   
 # cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1)
 @task(name="write_gcs", log_prints=True, tags="load_gcs", retries=3)
-def write_gcs(filepath: str):
+def write_gcs(filename: str, filepath: str):
+  """Uploads file from filepath to filename in GCS"""
   gcp_cloud_storage_bucket_block = GcsBucket.load("taxi-gcp")
-  path_uploaded_to = gcp_cloud_storage_bucket_block.upload_from_path(from_path=f"./week2/data_github/{filepath}.parquet", to_path=filepath)
-  print(f"Uploaded to {path_uploaded_to}")
+  path_uploaded_to = gcp_cloud_storage_bucket_block.upload_from_path(from_path=filepath, to_path=filename) # from_path=./week2/data_github/{filepath}.parquet
+  print(f"Uploaded to {path_uploaded_to} in GCS")
+  
+  
+  
+@flow(name="etl_parent_flow_large", log_prints=True)
+def etl_parent_flow_large(colour: str, year: int, months: list[int]):
+  for month in months:
+    etl_large(colour, year, month)
   
 @flow(name="etl_large", log_prints=True)
-def etl_large(url: str, filepath: str, colour: str):
-  filename = "yellow_tripdata_2019-03"
+def etl_large(colour: str, year: int, month: int):
+  filename = f"{colour}_tripdata_{year}-{month:02d}"
+  url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{colour}/{filename}.csv.gz"
+  filepath = f"./week2/data/{filename}.parquet"
+  
   df = pd.read_csv(url, nrows=10)
   df.head(0).to_parquet(filepath, index=False, engine="fastparquet")
   
@@ -70,8 +81,12 @@ def etl_large(url: str, filepath: str, colour: str):
     df.to_parquet(filepath, index=False, engine="fastparquet", append=True)
   print(f"locally downloaded {filename} and saved it as parquet")
   
-  write_gcs(filename)
+  write_gcs(f"{colour}/{filename}.parquet", filepath)
   
 if __name__ == "__main__":
   # etl_parent_flow(months=[4], colours=["green"], years=[2019])
-  etl_large("https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2019-03.csv.gz", "./week2/data/yellow_tripdata_2019-03.parquet", "yellow")
+  etl_parent_flow_large(colour="yellow", year="2019", months=list(range(1,13)))
+  etl_parent_flow_large(colour="yellow", year="2020", months=list(range(1,13)))
+  etl_parent_flow_large(colour="green", year="2019", months=list(range(1,13)))
+  etl_parent_flow_large(colour="green", year="2020", months=list(range(1,13)))
+  
